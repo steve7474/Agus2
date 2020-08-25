2478e0dffeebbb3880af716f8ee546ad395d22de
steve7474 <70121747+steve7474@users.noreply.github.com>
2020-08-25 15:34:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ create().then((client) => start(client));
 async function start(client) {
   client.onMessage(async (msg) => {
     // console.log(msg);
-    if (msg.body === "!cek") {
-      client.sendText(msg.from, "👋 Hello! BOT AKTIF");
+    if (msg.body === "!cek","#cek") {
+      client.sendText(msg.from, "👋 Hello! BOT STIKER SEDANG AKTIF");
       client;
-	if (msg.body === "!stiker") {
+	if (msg.body === "!stiker","#stiker","#STIKER","!STIKER") {
       client.sendText(msg.from, "Bantu saya untuk mengembangkan BOT ini dengan share");
       client;
+	}
     } else if (msg.mimetype) {
-      if (msg.caption === "!stiker" && msg.type === "image") {
+      if (msg.caption === "!stiker","#STIKER","#stiker","!STIKER" && msg.type === "image") {
         const mediaData = await decryptMedia(msg);
         const imageBase64 = `data:${msg.mimetype};base64,${mediaData.toString(
           "base64"
